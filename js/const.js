4fa47c8fed72590abed3a9fa32f4b77252f55aff
ferrangarciac <50447529+ferrangarciac@users.noreply.github.com>
2024-03-12 11:55:02 +0100

    Delete js directory

--- js/const.js
+++
@@ -1,9 +0,0 @@
-//minifier HTML: https://www.willpeavy.com/tools/minifier/
-//Covers de steam: https://steamcdn-a.akamaihd.net/steam/apps/xxxxxx/library_600x900_2x.jpg
-
-const PLATFORMS = {
-	COLOR: ["red", "blue", "yellow", "pink", "white", "green", "orange"],
-	NAME: ["AMSTRAD","SMS","GG","MEGADRIVE", "DC", "NES", "SNES", "N64", "NGC", "Wii", "Wii U", "Switch", "GB", "GBA", "NDS", "3DS", "WS", "PSOne", "PS2", "PS3", "PS4", "PS5", "PSP", "PSVITA", "Xbox360", "PC"],
-	NAMECOMPLETE: ["Amstrad CPC","Master System","Game Gear","Megadrive", "DreamCast", "NES", "Super Nintendo", "Nintendo 64", "Game Cube", "Wii", "Wii U", "Switch", "Gameboy / GB Color", "Gameboy Advance", "Nintendo DS", "Nintendo 3DS", "WS / WS Color", "Playstation", "Playstation 2", "Playstation 3", "Playstation 4", "Playstation 5", "PSP", "Playstation Vita", "Xbox 360", "PC"]
-}
-
