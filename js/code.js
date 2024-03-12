c9519163304702cec56580cc14f8b2d8264c2a64
ferrangarciac <50447529+ferrangarciac@users.noreply.github.com>
2024-03-12 11:55:19 +0100

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,233 @@
+var GAMES = new Array();
+
+async function getJsonInfo(path) {		//Lector de JSON
+    var response = await fetch(path);
+    var json = await response.json();
+	GAMES = json;
+	
+	//ESTADISTICAS
+	/*countGames();
+	showTags();
+	showLastGames();*/
+} 
+
+window.addEventListener('load', function() {
+	getJsonInfo("./js/db.json");
+	
+	//Rellena el menú de consolas
+	fillPlatformList();
+	
+	//Mostrar los últimos juegos
+	selectByData();
+}, false);
+
+function fillPlatformList(){
+	var platformMenu = document.getElementById("nav-platform-menu");
+	
+	for(i=0;i<platforms.NAMECOMPLETE.length; i++){
+		
+		var newPlatformItem = document.createElement("a");
+		newPlatformItem.innerHTML = platforms.NAMECOMPLETE[i];
+		newPlatformItem.className = "platform-item";
+		
+		newPlatformItem.platform = platforms.NAME[i].replace(/\s+/g, '').toLowerCase();
+		newPlatformItem.addEventListener('click', function(evt) {
+			selectByPlatform(evt);
+		}, false);
+		
+		platformMenu.appendChild(newPlatformItem);
+	}	
+}
+
+
+
+function selectByPlatform(evt){
+	var resultArray = new Array();
+	var cont = 0;
+	
+	var menuChk = document.getElementById("menu");
+	var platformChk = document.getElementById("platform-list");
+	
+	menuChk.checked = false;
+	platformChk.checked = false;
+	
+	var platform = evt.target.platform;
+	
+	for(var i = 0; i < GAMES.length; i++){
+		if(GAMES[i].platformId === platform){
+			resultArray[cont] = GAMES[i];
+			cont++;
+		}
+	}
+	
+	fillGameList(resultArray);
+}
+
+function selectByData(data){
+	var currentDate = new Date;
+	//Restamos un mes
+	currentDate.setDate(currentDate.getMonth() - 1);
+	
+	var cont = 0;
+	var resultArray = new Array();
+	
+	for(i=0;i<GAMES.length;i++){
+		if(Date.parse(GAMES[i].buyDate) > currentDate){
+			//console.log(GAMES[i].buyDate + " - " + currentDate);
+			resultArray[cont] = GAMES[i];
+			cont++;
+		}
+	}
+	
+	fillGameList(resultArray);
+}
+
+
+function fillGameList(arrayTemp){
+	var content = document.getElementById("content");
+	content.innerHTML = "";
+	
+	arrayTemp = arrayTemp.sort((a, b) => (a.gameName > b.gameName) ? 1 : -1);
+	
+	for(i=0;i<arrayTemp.length;i++){
+		var newGameContainer = document.createElement("div");
+		newGameContainer.className = "container-game-cont";
+		
+		newGameContainer.id = arrayTemp[i].id;
+
+		newGameContainer.addEventListener('click', fillGameDetails, false);
+		
+		var newGameCover = document.createElement("img");
+		newGameCover.className = "container-game-cover";
+		newGameCover.setAttribute("src", "./img/basiccover/" + arrayTemp[i].platformId + "/cover_" + arrayTemp[i].gameId + ".jpg");
+		
+		var newGameName = document.createElement("span");
+		newGameName.className = "container-game-name";
+		newGameName.innerHTML = arrayTemp[i].gameName;
+		
+		newGameContainer.appendChild(newGameCover);
+		newGameContainer.appendChild(newGameName);
+		
+		content.appendChild(newGameContainer);		
+	}
+}
+
+function fillGameDetails(evt){
+	var gameDetailsBox = document.getElementById("game");
+	gameDetailsBox.className = "game-info-cont";
+	
+	var backGameBox = document.getElementById("back-game-box");
+	backGameBox.className = "skip-game-box";
+	
+	backGameBox.addEventListener('click', skipGameInfo , false);
+	
+	var gameName = document.getElementById("game-name");
+	var gameCode = document.getElementById("game-code");
+	var gamePlatform = document.getElementById("game-platform");
+	var gameYear = document.getElementById("game-year");
+	var gameDeveloper = document.getElementById("game-developer");
+	var gameGenre = document.getElementById("game-genre");
+		
+	gameName.innerHTML = GAMES[this.id].gameName;
+	gameCode.innerHTML = GAMES[this.id].code;
+	gamePlatform.innerHTML = GAMES[this.id].platformId;
+	gameYear.innerHTML = GAMES[this.id].year;
+	gameDeveloper.innerHTML = GAMES[this.id].developer;
+	gameGenre.innerHTML = GAMES[this.id].genre;
+	
+	var gameRegion = document.getElementById("game-region");
+	var gameDisk = document.getElementById("game-owned");
+	var gameBox = document.getElementById("game-gamebox");
+	var gameManual = document.getElementById("game-manual");
+	var gameSaveFile = document.getElementById("game-save");
+	
+	gameDisk.className = "skip";
+	gameBox.className = "skip";
+	gameManual.className = "skip";
+	gameSaveFile.className = "skip";
+	
+	gameRegion.setAttribute("src", "./img/icons/icon_" + GAMES[this.id].region + ".png");
+	
+	if(GAMES[this.id].gameType == 1){
+		gameDisk.className = "game-icon";
+	}
+	
+	if(GAMES[this.id].box){
+		gameBox.className = "game-icon";
+	}
+	
+	if(GAMES[this.id].manual){
+		gameManual.className = "game-icon";
+	}
+	
+	if(GAMES[this.id].save){
+		gameSaveFile.className = "game-icon";
+	}
+	
+	var tags = document.getElementById("game-tags");
+	
+	tags.innerHTML = "";
+	
+	for(i=0;i<GAMES[this.id].tags.length;i++){
+		var newTag = document.createElement("span");
+		newTag.innerHTML = GAMES[this.id].tags[i]
+		
+		tags.appendChild(newTag);
+	}
+	
+}
+
+function skipGameInfo(){
+	var gameDetailsBox = document.getElementById("game");
+	gameDetailsBox.className = "skip";
+	
+	var backGameBox = document.getElementById("back-game-box");
+	backGameBox.className = "skip";
+}
+
+function generateBox(gameId){
+	var front = document.getElementById("front");
+	var back = document.getElementById("back");
+	var right = document.getElementById("right");
+	var left = document.getElementById("left");
+	
+	right.style.width = GAMES[gameId].boxSize[2] + "px";
+	right.style.height = GAMES[gameId].boxSize[1] + "px";
+	right.style.transform = "translateZ(" + (GAMES[gameId].boxSize[0])/2 + "px) translateX(" + (GAMES[gameId].boxSize[0])/2 + "px)";
+	right.style.backgroundImage = "url(./img/box-images/" + GAMES[gameId].gameId + "-" + GAMES[gameId].platformId + "-" + GAMES[gameId].edition + "-right.png)";
+	
+	left.style.width = GAMES[gameId].boxSize[2] + "px";
+	left.style.height = GAMES[gameId].boxSize[1] + "px";
+	left.style.transform = "translateZ(" + ((GAMES[gameId].boxSize[0])/2)*(-1) + "px) translateX(" + (GAMES[gameId].boxSize[0])/2 + "px)";
+	
+	if(GAMES[gameId].edition != "standart" || GAMES[gameId].platformId == "nes"){
+		left.style.backgroundImage = "url(./img/box-images/" + GAMES[gameId].gameId + "-" + GAMES[gameId].platformId + "-" + GAMES[gameId].edition + "-left.png)"; 
+	}else{
+		left.style.backgroundImage = "url(./img/box-images/" + GAMES[gameId].platformId + "-standart-left.png)"; 
+	}
+	
+	front.style.width = GAMES[gameId].boxSize[0] + "px";
+	front.style.height = GAMES[gameId].boxSize[1] + "px";
+	front.style.backgroundImage = "url(./img/box-images/" + GAMES[gameId].gameId + "-" + GAMES[gameId].platformId + "-" + GAMES[gameId].edition + "-front.png)"; 
+	
+	back.style.width = GAMES[gameId].boxSize[0] + "px";
+	back.style.height = GAMES[gameId].boxSize[1] + "px";
+	back.style.transform = "translateX(" + GAMES[gameId].boxSize[2] + "px) rotateY(90deg)";
+	back.style.backgroundImage = "url(./img/box-images/" + GAMES[gameId].gameId + "-" + GAMES[gameId].platformId + "-" + GAMES[gameId].edition + "-back.png)"; 
+	
+}
+
+/*function caca(){
+	var pene;
+	for(i=0;i<GAMES.length;i++){
+		//console.log("{'id': " + i + ", 'gameId': '" + GAMES[i].gameId + "','crown': '" + GAMES[i].crown + "','platformId': '" + GAMES[i].platformId + "','code': '" + GAMES[i].code + "','year': '" + GAMES[i].year + "','developer': '" + GAMES[i].developer + "','genre': '" + GAMES[i].genre + "','region': '" + GAMES[i].region + "','box': " + GAMES[i].box + ",'manual': " + GAMES[i].manual + ",'gameType': " + GAMES[i].gameType + ",'save': " + GAMES[i].save + ",'commentHTML': '" + GAMES[i].commentHTML + "','gameName': '" + GAMES[i].gameName + "','tags': ['" + GAMES[i].tags[0] + "'],   }");
+		pene = pene + "{'id': " + i + ", 'gameId': '" + GAMES[i].gameId + "','crown': '" + GAMES[i].crown + "','platformId': '" + GAMES[i].platformId + "','code': '','year': '','developer': '','genre': '','region': '" + GAMES[i].region + "', 'edition': 'standart', 'box': " + GAMES[i].box + ",'manual': " + GAMES[i].manual + ",'gameType': " + GAMES[i].gameType + ",'save': " + GAMES[i].save + ",'commentHTML': '" + GAMES[i].commentHTML + "','gameName': '" + GAMES[i].gameName + "', 'buyDate': '', 'startGame': '', 'endGame': '', 'price': 0, 'tags': ["
+		//'],   },";
+		for(x=0;x<GAMES[i].tags.length;x++){
+			pene = pene + "'" + GAMES[i].tags[x] + "'"
+		}
+	pene = pene + "] },";
+	}
+	
+	console.log(pene);
+}*/
---
+++ js/const.js
@@ -0,0 +1,64 @@
+const platforms = {
+	NAME: [
+		"AMSTRAD",    //00
+		"SMS",        //01
+		"GG",         //02
+		"MEGADRIVE",  //03
+		"DC",         //04
+		"NES",        //05
+		"SNES",       //06
+		"N64",        //07
+		"NGC",        //08
+		"Wii",        //09
+		"Wii U",      //10
+		"Switch",     //11
+		"GB",         //12
+		"GBA",        //13
+		"NDS",        //14
+		"3DS",        //15
+		"WS",         //16
+		"PSOne",      //17
+		"PS2",        //18
+		"PS3",        //19
+		"PS4",        //20
+		"PS5",        //21
+		"PSP",        //22
+		"PSVITA",     //23
+		"Xbox360",    //24
+		"PC",         //25
+	],
+	NAMECOMPLETE: [
+		"Amstrad CPC",					//00
+		"Master System",                //01
+		"Game Gear",                    //02
+		"Megadrive",                    //03
+		"DreamCast",                    //04
+		"NES",                          //05
+		"Super Nintendo",               //06
+		"Nintendo 64",                  //07
+		"Game Cube",                    //08
+		"Wii",                          //09
+		"Wii U",                        //10
+		"Switch",                       //11
+		"Gameboy / GB Color",           //12
+		"Gameboy Advance",              //13
+		"Nintendo DS",                  //14
+		"Nintendo 3DS",                 //15
+		"WS / WS Color",                //16
+		"Playstation",                  //17
+		"Playstation 2",                //18
+		"Playstation 3",                //19
+		"Playstation 4",                //20
+		"Playstation 5",                //21
+		"PSP",                          //22
+		"Playstation Vita",             //23
+		"Xbox 360",                     //24
+		"PC",                           //25
+	]
+}
+
+/*const standartBoxSize{
+	PS4: [135,170,15],
+	SWITCH: [105,170,12],
+	NES: [126,177,23]
+}*/
